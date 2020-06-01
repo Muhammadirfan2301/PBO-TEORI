a38c58e537830df5df9282d443e21f643d733400
Muhammadirfan2301 <66212465+Muhammadirfan2301@users.noreply.github.com>
2020-06-01 12:15:42 +0700

    Add files via upload

---
+++ ContohFinal.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wrapper_dan_final;
+
+/**
+ *
+ * @author IRFAN
+ */
+
+public final class finalmobil {
+    
+    private String nama;
+    
+    public String getNama() {
+        return nama;
+    }
+ 
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+}
